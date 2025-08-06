73226507c807b47858b2c4775a194c0826028e78
umgaddafi <gaddafiumar4445@gmail.com>
2025-08-06 20:59:33 +0100

    first App  release

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from src.splash_screen import SplashScreen
-from src.core_crypto import 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     splash = SplashScreen()
